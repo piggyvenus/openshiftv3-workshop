d4d190cbff5b26a62d02c5401818bf5280d33367
Samuel Tauil <samueltauil@gmail.com>
2017-05-18 00:28:23 -0400

    cosmetic

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git add .
+git commit -m "cosmetic"
+git push
+oc delete all --all -n labs
+oc new-app osevg/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/samueltauil/openshiftv3-workshop/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/samueltauil/openshiftv3-workshop/master/_module_groups.yml"
