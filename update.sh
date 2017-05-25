702e700dbce0b01aefd0090140c352c996849948
Samuel Tauil <samueltauil@gmail.com>
2017-05-25 11:47:08 -0400

    dynamic commit messages

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git add .
-git commit -m "cosmetic"
+git commit -am "$(curl -s http://whatthecommit.com/index.txt 2>&1)"
 git push
 oc delete all --all -n labs
 oc new-app --name=workshop samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/samueltauil/openshiftv3-workshop/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/samueltauil/openshiftv3-workshop/master/_module_groups.yml"
