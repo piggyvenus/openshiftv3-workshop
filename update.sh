3367ef1dab3cfddc99275f1a052f53154c8b10e0
piggyvenus <piggyvenus@gmail.com>
2018-02-10 19:51:46 -0800

    3.7 workshop materials

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ git commit -am "$(curl -s http://whatthecommit.com/index.txt 2>&1)"
 git push
 oc delete all --all -n labs
 oc new-app --name=workshop samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/master/_module_groups.yml"
+oc new-app --name=workshop samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/v3.7" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/v3.7/_module_groups.yml"
