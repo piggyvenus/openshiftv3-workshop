0a5dc2ce6760773862fe28b221d930dd0ffeeb4f
piggyvenus <piggyvenus@gmail.com>
2018-05-15 18:00:53 -0700

    3.9 workshop

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ oc delete all --all -n labs
 oc new-app --name=lab samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/master/_module_groups.yml"
 oc new-app --name=workshop samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/v3.7" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/v3.7/_module_groups.yml"
 oc new-app --name=workshop samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv3-workshop/3.7" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv3-workshop/3.7/_module_groups.yml"
-oc new-app --name=lab samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/3.9" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/3.9:/_module_groups.yml"
+oc new-app --name=lab samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/3.9" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/piggyvenus/openshiftv3-workshop/3.9/_module_groups.yml"
 
